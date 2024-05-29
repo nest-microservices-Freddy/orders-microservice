24559aaf67c8af85273ce1470d6aa088887a9c04
fredsalv01 <freddy.morales@usil.pe>
2024-05-29 15:38:00 -0500

    create order details

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -15,9 +15,10 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   create(createOrderDto: CreateOrderDto) {
-    return this.order.create({
-      data: createOrderDto,
-    });
+    return {
+      service: 'Order microservice',
+      createOrderDto,
+    };
   }
 
   async findAll(orderPaginationDto: OrderPaginationDto) {
